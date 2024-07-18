4876a65861ed1c4d9c843f3adc1ab121942b6ccc
Chris Sammy <sammy.chrisammy.chris30@gmail.com>
2024-07-18 15:04:58 -0700

    2-append_write

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ defining append_write function"""
 
+
 def append_write(filename="", text=""):
     """append filename with utf-8"""
     with open(filename, "a", encoding="utf-8") as f:
