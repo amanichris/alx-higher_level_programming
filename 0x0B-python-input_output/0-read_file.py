a500689f94f0c0d30554432d9b0f7a4a7887c4ae
Chris Sammy <sammy.chrisammy.chris30@gmail.com>
2024-07-18 15:01:22 -0700

    0-read_file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ defining read_file function"""
 
+
 def read_file(filename=""):
     """function that reads a text file (UTF-8) and prints it to stdout"""
     with open(filename, encoding='utf-8') as f:
