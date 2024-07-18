e863510e5c76c3834ae6afbe136391c5d78303d2
Chris Sammy <sammy.chrisammy.chris30@gmail.com>
2024-07-18 15:11:18 -0700

    3-to_json_string

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-""" defining to_json_string function"""
+"""defining to_json_string function"""
+
 
 def to_json_string(my_obj):
-    """returns json representation of an object"""
-    return json.dumps (my_obj)
+     """returns json representation of an object"""
+    return json.dumps(my_obj)
